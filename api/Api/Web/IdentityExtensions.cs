a494f0952366448968f3acc6e300463c683dbe74
todd <todd+github@goneopen.com>
2018-07-01 16:06:36 +1200

    Add Auth0 beare authenticate advertising on 401

--- api/Api/Web/IdentityExtensions.cs
+++ api/Api/Web/IdentityExtensions.cs
@@ -6,6 +6,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Infrastructure.mySql;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
@@ -89,9 +90,72 @@ namespace Api.Web
                     options.Authority = domain;
                     options.Audience = configuration["Auth0:Audience"];
 
+
+                    // see https://www.jerriepelser.com/blog/accessing-tokens-aspnet-core-2/
+                    //
+                    // Set to true if you want to get the value of the access_token in one of your
+                    // controller actions, you will need to retrieve the AuthenticateInfo. The access_token
+                    // will be stored inside the Properties with the key “.Token.access_token”:
+                    //
+                    // eg for this scheme "JSONWebToken"
+                    //
+                    //    string accessToken = await HttpContext.GetTokenAsync("access_token");
+                    //
+/*
+                    options.SaveToken = true;
+*/
+
                     // TODO: inject api hosting address and uri construction
                     options.Challenge =
                         $"{Auth0AuthenticationSchemeName} realm=\"api\", uri=http://localhost:5000/authenticate/auth0";
+
+                    options.Events = new JwtBearerEvents
+                    {
+                        /**
+                         * Read a custom authorization header for scheme name: JSONWebToken
+                         *
+                         * example:
+                         *    Authorization: JSONWebToken eyJ0eXAiOiJKV1QiLCJhbG...
+                         *
+                         * Default implementation is to look for scheme: Bearer (see https://github.com/aspnet/Security/blob/master/src/Microsoft.AspNetCore.Authentication.JwtBearer/JwtBearerHandler.cs#L63)
+                         *
+                         * This uses the event OnMessageReceived to process the header and pass it into the pipeline.
+                         *   see event trigger: https://github.com/aspnet/Security/blob/master/src/Microsoft.AspNetCore.Authentication.JwtBearer/JwtBearerHandler.cs#L54
+                         *
+                         *
+                         * see https://andrewlock.net/a-look-behind-the-jwt-bearer-authentication-middleware-in-asp-net-core/#readauthorizationheader
+                         * related see https://stackoverflow.com/questions/41955117/custom-token-location-for-jwtbearermiddleware
+                         */
+                        OnMessageReceived = context =>
+                        {
+                            if (string.IsNullOrEmpty(context.Token))
+                            {
+                                string authorization = context.Request.Headers["Authorization"];
+
+                                // If no authorization header found, nothing to process further
+                                if (string.IsNullOrEmpty(authorization))
+                                {
+                                    return Task.FromResult(AuthenticateResult.NoResult());
+                                }
+
+                                // note the space in the prefix
+                                var schemePrefx = $"{Auth0AuthenticationSchemeName} ";
+                                if (authorization.StartsWith(schemePrefx, StringComparison.OrdinalIgnoreCase))
+                                {
+                                    // this then gets picked up further in the pipeline to be processes
+                                    context.Token = authorization.Substring(schemePrefx.Length).Trim();
+                                }
+
+                                // If no token found, no further work possible
+                                if (string.IsNullOrEmpty(context.Token))
+                                {
+                                    return Task.FromResult(AuthenticateResult.NoResult());
+                                }
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
                 })
                 .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
                 {
@@ -110,6 +174,7 @@ namespace Api.Web
                     };
                 })
                 ;
+/*
 
             // see https://auth0.com/docs/quickstart/backend/aspnet-core-webapi/01-authorization#configure-the-sample-project
             // allows profiles on [Authorize]
@@ -122,6 +187,7 @@ namespace Api.Web
 
             // register the scope authorization handler
             services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
+*/
 
             return services;
         }
@@ -227,7 +293,7 @@ namespace Api.Web
     public class HasScopeHandler : AuthorizationHandler<HasScopeRequirement>
     {
         private const string Scope = "scope";
-        
+
         protected override Task HandleRequirementAsync(
             AuthorizationHandlerContext context,
             HasScopeRequirement requirement)
