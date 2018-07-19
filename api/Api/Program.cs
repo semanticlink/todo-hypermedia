b31bfb55034e2022cadea63959dccba03844f2f9
todd <todd+github@goneopen.com>
2018-07-20 07:37:38 +1200

    Setup logging in Api

--- api/Api/Program.cs
+++ api/Api/Program.cs
@@ -1,13 +1,37 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
+using Toolkit;
 
 namespace Api
 {
     public class Program
     {
+        private static readonly NLog.ILogger Log = NLogBuilder
+            .ConfigureNLog("Config/NLog.config")
+            .GetCurrentClassLogger();
+
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            try
+            {
+                Log.Debug("[Init] starting");
+                BuildWebHost(args).Run();
+            }
+            catch (Exception ex)
+            {
+                Log.ErrorExceptionFormat(ex, "[Init] stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                Log.Debug("[Init] shutting down");
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                LogManager.Shutdown();
+            }
         }
 
         private static IWebHost BuildWebHost(string[] args) =>
@@ -17,6 +41,15 @@ namespace Api
                  */
                 .CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    // remove all other loggers
+                    logging.ClearProviders();
+                    // setup log levels for other
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                // Register NLog other code using Microsoft.Extensions.ILogger
+                .UseNLog()
                 .Build();
     }
 }
