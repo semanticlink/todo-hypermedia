e2674334461a4ec11d4552dfa961cf0322e79de7
todd <todd+github@goneopen.com>
2018-07-22 18:44:57 +1200

    Add patch creation

--- client/src/components/DragDroppableCollection.vue
+++ client/src/components/DragDroppableCollection.vue
@@ -10,9 +10,10 @@
     </drag-and-droppable-model>
 
     <drag-and-droppable-model
-            v-if="hasPatchWIthJsonPatch"
+            v-if="hasPatchWithJsonPatch"
             :model="representation"
-            media-type="application/json-patch+json"
+            media-type="text/uri-list"
+            :dropped="addAsPatch"
     >
         <b-button size="sm" variant="primary">Patch</b-button>
     </drag-and-droppable-model>
@@ -23,9 +24,11 @@
 
     import DragAndDroppableModel from './DragAndDroppableModel.vue';
 
-    import {matches, get, put, getUri} from 'semantic-link';
+    import {matches, get, put, getUri, link} from 'semantic-link';
     import {log} from 'logger';
     import {fromUriList, makeUriList} from "../lib/util/dragAndDropModel";
+
+    import {compare, deepClone} from 'fast-json-patch'
 
     export default {
         name: 'drag-droppable-collection',
@@ -44,7 +47,7 @@
             hasPutWithUriList() {
                 return matches(this.representation, 'edit-form', 'text/uri-list');
             },
-            hasPatchWIthJsonPatch() {
+            hasPatchWithJsonPatch() {
                 return matches(this.representation, 'edit-form', 'application/json-patch+json');
             }
         },
@@ -72,8 +75,39 @@
                         });
 
                     })
-                    .catch(/** @type {AxiosError} */response => {
-                        this.$notify({text: response, type: 'error'});
+                    .catch(/** @type {AxiosError} */error => {
+                        this.$notify({text: error.statusText, type: 'error'});
+                    })
+            },
+            /**
+             * Update the collection by adding a new uri to the existing items via Json Patch Document and put back to the
+             * 'submit' rel on the form (or the collection if not present)
+             * @param {string} document uri-list of the resources to be added to the collection
+             */
+            addAsPatch: function (document) {
+
+                log.debug(`Adding uri-list: ${document}`);
+
+                const newCollection = deepClone(this.representation);
+
+                // add to the collection
+                fromUriList(document).forEach(uri => newCollection.items.push({id: uri}));
+
+                const patch = compare(this.representation, newCollection);
+
+                get(this.representation, 'edit-form', 'application/json-patch+json')
+                    .then(response => {
+                        return link(response.data, 'submit', 'application/json-patch+json', 'PATCH', patch /*JSON.stringify(patch)*/);
+                    })
+                    .then(response => {
+                        this.$notify({
+                            text: `${response.statusText} <a href="${response.headers.location}">item<a>`,
+                            type: 'success'
+                        });
+
+                    })
+                    .catch(/** @type {AxiosError} */error => {
+                        this.$notify({text: error.statusText, type: 'error'});
                     })
             }
         }
